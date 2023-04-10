cbea6e505a9dbf59126a68b5d6fb300efc9e97c6
macmc <macmc@macmcs-MacBook-Pro.local>
2023-04-10 11:36:09 -0500

    init

---
+++ src/main/java/com/builterdev/apiblog/ApIblogApplication.java
@@ -0,0 +1,13 @@
+package com.builterdev.apiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApIblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/builterdev/apiblog/ApIblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.builterdev.apiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
