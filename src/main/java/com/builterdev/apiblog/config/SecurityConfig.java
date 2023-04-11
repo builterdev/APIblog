3aa6de54db51502dfe7f86da0a772483c2e1bbaa
AaronHapy <aaroncerceda26@gmail.com>
2023-04-10 22:06:24 -0500

    added DB authentication instead of in-memory

--- src/main/java/com/builterdev/apiblog/config/SecurityConfig.java
+++ src/main/java/com/builterdev/apiblog/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.builterdev.apiblog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +19,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -34,7 +47,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(){
         UserDetails normal = User.builder()
                 .username("aaron")
@@ -49,6 +62,6 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(normal, admin);
-    }
+    }*/
 
 }
---
+++ src/main/java/com/builterdev/apiblog/utils/PasswordEncoderGenerator.java
@@ -0,0 +1,15 @@
+package com.builterdev.apiblog.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderGenerator {
+
+    public static void main(String[] args) {
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(passwordEncoder.encode("aaron12345"));
+    }
+
+}
