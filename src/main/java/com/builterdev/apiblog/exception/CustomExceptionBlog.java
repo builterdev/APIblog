bcb7ae55d1ae3b663e41349cde9336a056420341
AaronHapy <aaroncerceda26@gmail.com>
2023-04-11 20:47:37 -0500

    removed unused import

--- src/main/java/com/builterdev/apiblog/exception/CustomExceptionBlog.java
+++ src/main/java/com/builterdev/apiblog/exception/CustomExceptionBlog.java
@@ -1,7 +1,6 @@
 package com.builterdev.apiblog.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class CustomExceptionBlog extends RuntimeException{
 
